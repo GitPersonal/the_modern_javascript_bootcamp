061f28ec6cd0078081851ff373a76ea11de3d4ab
GitPersonal <juanchavez11@yahoo.com>
2019-06-12 16:31:21 -0500

    Update boolean-advanced.js

--- basics/boolean-advanced.js
+++ basics/boolean-advanced.js
@@ -1,3 +1,4 @@
+// example 1
 let isAccountLocked = true
 
 //if account is locked print 'account is locked' else print 'welcome to your account'
@@ -5,3 +6,92 @@ if (isAccountLocked){
   console.log('Is account locked')
 } else{
 console.log('Welcomr e to your account')
+
+// example 2
+let isAccountLocked = true
+let userRole = "admin"
+
+//if account is locked print 'account is locked' else print 'welcome to your account'
+if (isAccountLocked){
+  console.log('Is account locked')
+} else if () {
+    
+} else{
+console.log('Welcomr e to your account')
+  
+//   -------------------------------------
+//   *********************************
+  //   -------------------------------------
+  
+if (isAccountLocked) {
+  console.log('Is account locked')
+} else if(userRole === "admin") {
+  console.log("Welcome Admin")
+} else{
+  console.log('Welcomr e to your account')
+}
+  
+  
+  
+  
+  //   *********************************
+  
+//   example 3
+  if (true) {
+    //code stops running here
+  }else if(true) {
+  
+  } else{
+  
+  }
+  
+  
+//   *********************************
+  
+  //   example 4
+  if (false) {
+    //code continues running
+  }else if(true) {
+  //code stops running here
+  } else{
+  
+  }
+  //   *********************************
+  
+  //   example 5
+  if (false) {
+    //code continues running
+  }else if(false) {
+    //code continues running
+  } else{
+  //code stops running here
+  }
+  
+  
+//   ======================================
+//            Challenge Area
+//   ======================================
+  // It is freezong outside
+  // It is hot outside
+  // Go for it. It is prety nice
+  
+  let temp = 45
+  let isFreezing = 32
+  let isHot = true
+  
+  if(temp <= 32 ){
+    console.log("It is freezong outside")
+  } if else (temp >= 100)  {
+    console.log("It is hot outside")
+  } else {
+    console.log("Go for it. It is prety nice")
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
