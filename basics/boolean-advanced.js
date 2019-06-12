21bbe67fbd430ce333411c100368015aaf36553e
GitPersonal <juanchavez11@yahoo.com>
2019-06-12 16:00:24 -0500

    Create boolean-advanced.js

---
+++ basics/boolean-advanced.js
@@ -0,0 +1,7 @@
+let isAccountLocked = true
+
+//if account is locked print 'account is locked' else print 'welcome to your account'
+if (isAccountLocked){
+  console.log('Is account locked')
+} else{
+console.log('Welcomr e to your account')
